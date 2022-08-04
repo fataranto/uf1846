4c835c4e10b7f6c4a82528a1478f2b80dccdf01f
fataranto <fataranto@gmail.com>
2022-08-04 17:44:32 +0200

    hasta ex 8 ok

--- estudiants_exercicis_1-8/controllers/countryControllers.js
+++ estudiants_exercicis_1-8/controllers/countryControllers.js
@@ -5,11 +5,29 @@ exports.getHomePage = (req, res) => {
 }
 
 exports.postSendData = (req, res) => {
-    const fakeData = {
+    //console.log(req.body);
+    const data = {
+        name: req.body.country,
+        population: req.body.population,
+        flagURL: req.body.urlFlag
+    }
+    
+    Country.create(data, (error, country) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        console.log(country);
+        res.redirect('/');
+    } );
+
+
+
+    /* const fakeData = {
         conuntryName: "spain",
         population: 46438420,
         urlFlag: "https://restcountries.eu/data/esp.svg"
-    }
+    } */
 
-    res.send("Dades rebudes:" + JSON.stringify(fakeData));
+   // res.send("Dades rebudes:" + JSON.stringify(data));
 }
